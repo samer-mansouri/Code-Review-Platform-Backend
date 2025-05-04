4e02ca753a9e6d61106f442351d9cc0757d74182
samer-mansouri <samer.mansouri@insat.ucar.tn>
2025-05-04 15:47:36 +0100

    config.py file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = 900
+    JWT_REFRESH_TOKEN_EXPIRES = 2592000
+    MONGODB_SETTINGS = {
+        'db': os.getenv("MONGO_DB"),
+        'host': os.getenv("MONGO_URI")
+    }
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2 MB
+    UPLOAD_FOLDER = 'app/uploads'
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
